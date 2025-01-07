c3e297acf13e71f20a8ca2086b3f1767a5f5a2ef
Spicfy <68024083+Spicfy@users.noreply.github.com>
2025-01-07 15:54:26 -0500

    updated backend routes and api

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,45 @@
+const asyncHandler = require('express-async-handler')
+
+
+
+// @desc Get all goals
+// @route GET /api/goals
+// @access Private
+const getGoals = asyncHandler(async (req, res) => {
+
+    res.status(200).json({message: 'Get goals'})
+})
+
+// @desc set all goals
+// @route POST /api/goals
+// @access Private
+const setGoal = asyncHandler(async(req, res) => {
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
+    res.status(201).json({message: 'Set goals'})
+})
+
+// @desc upate all goals
+// @route PUT /api/goals/:id
+// @access Private
+const updateGoal = asyncHandler(async (req, res) => {
+    
+    res.status(200).json({message: `Update goal ${req.params.id}`})
+})
+
+// @desc Delete goal
+// @route DELETE /api/goals/:id
+// @access Private
+const deleteGoal = asyncHandler(async (req, res) => {
+
+    res.status(200).json({message: `Delete goal ${req.params.id}`})
+})
+
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+}
